ee1daf0f9c300a6253df4faba21381b7bfcea743
J-Algiere <algiere.jarad@gmail.com>
2025-07-06 16:39:38 -0500

    Commit 1

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 """
 Inheritance (Object-Oriented Programming)
 """
-def main():
-    dog = Dog("Raven", "Pitbull", "1")
-    print(f'{dog.name} is a {dog.breed} and is {dog.age} year(s) old.')
-    print(dog.speak())
-
 class Pet:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
     def speak(self) -> str:
-        return "Some genric pet sound"
+        return "Some generic pet sound"
 
 class Dog(Pet):
-    def __init__(self,name, breed, age):
-        super().__init__(name, age) # call the pets __init__
+    def __init__(self, name, age, breed):
+        super().__init__(name, age)  # Inherit name and age
         self.breed = breed
 
     def speak(self) -> str:
         return "Woof!"
 
+class Cat(Pet):
+    def __init__(self, name, age, breed):
+        super().__init__(name, age)
+        self.breed = breed
+
+    def speak(self) -> str:
+        return "Meow!"
+
+def main():
+    dog = Dog("Raven", 2, "Pitbull")
+    cat = Cat("Olive", 1, "Tabby")
+    print(f"{dog.name} is a {dog.breed}, {dog.age} year(s) old.")
+    print(f"{dog.name} says: {dog.speak()}")
+    print(f"{cat.name} is a {cat.breed}, {cat.age} year(s) old.")
+    print(f'{cat.name} says: {cat.speak()}')
 
 if __name__ == "__main__":
     main()
