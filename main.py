2ecf7e64dacc97404e7227dfd49895ef085b7ee5
J-Algiere <algiere.jarad@gmail.com>
2025-07-07 18:16:48 -0500

    Commit 2

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 """
 Inheritance (Object-Oriented Programming) with a List of Pets
 """
+from abc import ABC, abstractmethod
+import json
 
-class Pet:
+class Pet(ABC):
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    @abstractmethod
     def speak(self) -> str:
-        return "Some generic pet sound"
+        pass
+    @abstractmethod
+    def to_dict(self):
+        pass
 
 class Dog(Pet):
+
     def __init__(self, name, age, breed):
         super().__init__(name, age)
         self.breed = breed
@@ -18,13 +25,32 @@ class Dog(Pet):
     def speak(self) -> str:
         return "Woof!"
 
+    def to_dict(self):
+        return \
+            {
+                "petType": type(self).__name__,
+                "name": self.name,
+                "age": self.age,
+                "breed": self.breed
+            }
+
 class Cat(Pet):
+
     def __init__(self, name, age, breed):
         super().__init__(name, age)
         self.breed = breed
 
     def speak(self) -> str:
         return "Meow!"
+
+    def to_dict(self):
+        return \
+            {
+                "petType": type(self).__name__,
+                "name": self.name,
+                "age": self.age,
+                "breed": self.breed
+            }
 
 def main():
     # Create a list to hold all pets
@@ -38,11 +64,19 @@ def main():
 
     # Loop through the pets and print info
     print("---- Pet Roster ----")
-    for pet in pets:
+    for pet in pets :
         print(f"{pet.name} ({type(pet).__name__}) - Age: {pet.age}")
         print(f"Breed: {pet.breed}")
         print(f"{pet.name} says: {pet.speak()}")
         print()
+
+    # Create a list of Dictionaries for pets list
+    pet_dict = [pet.to_dict()for pet in pets]
+
+
+    with open("pets.json", "w") as json_file:
+        json.dump(pet_dict, json_file, indent=4)
+        print(json.dumps(pet_dict, indent=4))
 
 if __name__ == "__main__":
     main()
